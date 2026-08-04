37381e0164ec5a24fcd600fcec1fea4ca7fea509
lilithdotgov <lilithj2027@gmail.com>
2026-08-04 15:09:30 -0400

    Accelerometer and Sleep cycle functional

--- C port/BWC/BWC.c
+++ C port/BWC/BWC.c
@@ -1,56 +1,101 @@
 #include <stdio.h>
 #include <string.h>
+
 #include "pico/stdlib.h"
+#include "pico/low_power.h"
+#include "pico/status_led.h"
 #include "hardware/spi.h"
+#include "hardware/powman.h"
+
 #include "accelerometer.h"
 
-uint8_t data[STRIKE_SAMPLES * BPS];
+/*
+Notes on LEDs (specifically status_led_init):
+Initialize the status LED(s) and the resources they need before use. On some devices (e.g. Pico W, Pico 2 W) accessing
+the status LED requires talking to the WiFi chip, which requires an async_context. This method will create an
+async_context for you.
+However an application should only use a single async_context instance to talk to the WiFi chip. If the application
+already has an async context (e.g. created by cyw43_arch_init) you should use status_led_init_with_context instead and
+pass it the async_context already created by your application
+*/
 
-int main(){
+uint8_t __persistent_data(data)[STRIKE_SAMPLES * BPS];
+
+// Following is a dummy variable for debug purposes
+static uint32_t __persistent_data(run_count); // Un-Initialized static variables guarenteed to be 0
+
+void return_func(pstate_bitset_t *pstate) {
+    run_count++;
+}
+
+int main() {
     stdio_init_all();
 
     gpio_init(PIN_INTR);
     gpio_set_dir(PIN_INTR, GPIO_IN);
-    
+
     gpio_init(PIN_CS);
     gpio_set_dir(PIN_CS, GPIO_OUT);
     gpio_set_pulls(PIN_CS, true, false);
-    gpio_put(PIN_CS, 1); //Must occur first before setting polarity and phase of SPI
-    
-    spi_init(SPI_PORT, 2000000); //2Mhz baudrate, see doc pg. 13 for info on appropriate ranges 
+    gpio_put(PIN_CS, 1); // Must occur first before setting polarity and phase of SPI
 
-    spi_set_format(SPI_PORT, 8, 1, 1, SPI_MSB_FIRST); //sets polarity and phase to 1 as required, see doc pg. 13
-    //Since pins on the board have specific SPI functions, you need not specify the role of each pin, it just works 
+    spi_init(SPI_PORT, 2000000); // 2Mhz baudrate, see doc pg. 13 for info on appropriate ranges
+
+    spi_set_format(SPI_PORT, 8, 1, 1, SPI_MSB_FIRST); // sets polarity and phase to 1 as required, see doc pg. 13
+    // Since pins on the board have specific SPI functions, you need not specify the role of each pin, it just works
 
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    //gpio_set_function(PIN_CS, GPIO_FUNC_SPI);
+    // gpio_set_function(PIN_CS, GPIO_FUNC_SPI);
 
-    
-
+    if (gpio_get(PIN_INTR)) {
+        fetch_data(data);
+    }
 
     read_state_on();
-    sleep_ms(100); //ADXL343 needs ~1.4ms to turn read state on, this is overkill just for demonstration
-    
-    fetch_data(data);
-    
-    for(int i = 0; i < STRIKE_SAMPLES * BPS; i += BPS){
-        sleep_ms(1000);
-        
-        // Combine LSB (data[i]) and MSB (data[i+1]), then cast to signed 16-bit integer
-        int16_t x = (int16_t)((data[i+1] << 8) | data[i]);
-        int16_t y = (int16_t)((data[i+3] << 8) | data[i+2]);
-        int16_t z = (int16_t)((data[i+5] << 8) | data[i+4]);
-        
-        printf("X: %d\t Y: %d\t Z: %d\n", x, y, z);
-    }
-    
-    
-    
-    while(true){
-        sleep_ms(1000);
-        printf("Response: %d\n", reading_validation());
+    sleep_ms(10); // ADXL343 needs at least ~1.4ms to turn setting on
+
+    intr_state_on();
+    sleep_ms(10); // ADXL343 needs at least ~1.4ms to turn setting on
+
+    // Specify which domains to go to *sleep* by adding all bitflags then removing sleeping domains
+    // We want SRAM0 to be awake for persistant memory so we don't remove it
+    pstate_bitset_t pstate = pstate_bitset_all();
+    pstate_bitset_remove(&pstate, POWMAN_POWER_DOMAIN_SRAM_BANK1);
+    pstate_bitset_remove(&pstate, POWMAN_POWER_DOMAIN_SWITCHED_CORE);
+    pstate_bitset_remove(&pstate, POWMAN_POWER_DOMAIN_XIP_CACHE);
+
+    if (gpio_get(PIN_INTR)) {
+        reset_intr_state();
     }
 
+    low_power_pstate_until_gpio_pin_state(PIN_INTR, false, true, &pstate, return_func);
 }
+
+/*
+   hard_assert(status_led_init());
+   if (run_count > 0) {
+       for (int i = 0; i < run_count; i++) {
+           status_led_set_state(true);
+           sleep_ms(1000 / run_count);
+           status_led_set_state(false);
+           sleep_ms(100 / run_count);
+       }
+   }
+   */
+
+/*
+        printf("Time to send data!\n");
+        sleep_ms(2000);
+        for (int i = 0; i < STRIKE_SAMPLES * BPS; i += BPS) {
+            sleep_ms(100);
+
+            // Combine LSB (data[i]) and MSB (data[i+1]), then cast to signed 16-bit integer
+            double x = G * (double)(int16_t)((data[i + 1] << 8) | data[i]) / 256.0;
+            double y = G * (double)(int16_t)((data[i + 3] << 8) | data[i + 2]) / 256.0;
+            double z = G * (double)(int16_t)((data[i + 5] << 8) | data[i + 4]) / 256.0;
+
+            printf("X: %f\t Y: %f\t Z: %f\n", x, y, z);
+        }
+*/
--- C port/BWC/accelerometer.c
+++ C port/BWC/accelerometer.c
@@ -1,8 +1,8 @@
+#include "accelerometer.h"
+#include "hardware/spi.h"
+#include "pico/stdlib.h"
 #include <stdio.h>
 #include <string.h>
-#include "pico/stdlib.h"
-#include "hardware/spi.h"
-#include "accelerometer.h"
 
 /*
 Documentation: https:www.analog.com/media/en/technical-documentation/data-sheets/adxl343.pdf
@@ -14,34 +14,34 @@ CS pin needs to be high voltage at start, see doc pg. 13
 SCK also needs to idle at high, see doc pg. 13
 */
 
-//Important registers:
-#define REG_DEVID 0x00 //Used to test that we are reading from correct spot. Check doc pg. 23
-#define DEVID 0xE5 //Should recieve this default value when reading the Device ID Register (REG_DEVID)
-#define REG_POWER_CTL 0x2D //Power Control Register, used to enable/disable data output
-#define REG_DATA_RATE 0x2C //Rate of output data
-#define DATA_RATE 0b1101 //0b1101 is 800RATE Data Output Rate, see doc pg. 12 for more ranges
-#define REG_DATAX0 0x32 //0x32 to 0x37 contain the data for each axis, each is 2 bytes. Order is x, y, and z
-#define REG_INT_ENABLE 0x2E //Interrupt Enable Register
-#define INT_ENABLE 0x10 //Enables Activity mode
-#define REG_ACT_CTL 0x27 //Enables which axis to be monitored for the interrupt modes. Plus an unused bonus feature, see doc pg. 23
-#define ACT_CTL 0b00010000 //Selects which axis to turn on for the interrupt. See doc pg. 23
-#define REG_INT_MAP 0x2F //Chooses which pin(s) to use for interrupts
-#define INT_MAP 0xEF //Sets activity mode interrupt to pin INT1
-#define REG_THRESH_ACT 0x24 //Sets threshold for interrupt to occur. Single unsigned byte. threshold = 62.5mg * THRESH_ACT.
-#define THRESH_ACT 0x08 //0x20 = 32, 32 * 62.5mg = 2g 
-#define REG_FIFO_MODE 0x38 //Register controlling FIFO modes
-#define FIFO_MODE 0x80 //Sets FIFO mode to stream
-#define REG_INT_SOURCE 0x30 //Read-only register, shows which interrupts were activated. Reading this resets the interrupt states
-#define REG_DATA_FORMAT 0x31 //Used for formatting data, see pg. 26
-#define DATA_FORMAT 0b00001010 //Importantly sets the data range, has other functionalities that are unused
+// Important registers:
+#define REG_DEVID       0x00       // Used to test that we are reading from correct spot. Check doc pg. 23
+#define DEVID           0xE5       // Should recieve this default value when reading the Device ID Register (REG_DEVID)
+#define REG_POWER_CTL   0x2D       // Power Control Register, used to enable/disable data output
+#define REG_DATA_RATE   0x2C       // Rate of output data
+#define DATA_RATE       0b1101     // 0b1110 is 1600hz Data Output Rate, see doc pg. 12 for more ranges
+#define REG_DATAX0      0x32       // 0x32 to 0x37 contain the data for each axis, each is 2 bytes. Order is x, y, and z
+#define REG_INT_ENABLE  0x2E       // Interrupt Enable Register
+#define INT_ENABLE      0x10       // Enables Activity mode
+#define REG_ACT_CTL     0x27       // Enables which axis to be monitored for the interrupt modes. Plus an unused bonus feature, see doc pg. 23
+#define ACT_CTL         0b00010000 // Selects which axis to turn on for the interrupt. See doc pg. 23
+#define REG_INT_MAP     0x2F       // Chooses which pin(s) to use for interrupts
+#define INT_MAP         0xEF       // Sets activity mode interrupt to pin INT1
+#define REG_THRESH_ACT  0x24       // Sets threshold for interrupt to occur. Single unsigned byte. threshold = 62.5mg * THRESH_ACT.
+#define THRESH_ACT      0x08       // 0x20 = 32, 32 * 62.5mg = 2g
+#define REG_FIFO_MODE   0x38       // Register controlling FIFO modes
+#define FIFO_MODE       0x80       // Sets FIFO mode to stream
+#define REG_INT_SOURCE  0x30       // Read-only register, shows which interrupts were activated. Reading this resets the interrupt states
+#define REG_DATA_FORMAT 0x31       // Used for formatting data, see pg. 26
+#define DATA_FORMAT     0b00001010 // Importantly sets the data range, has other functionalities that are unused
 
-//RP2350 runs at 125MHz by default on SDK, so each CPU cycle is 8ns
-//Need at least 150ns of wait for the longest SPI action
-//Altho SPI hardware should handle that
+// RP2350 runs at 125MHz by default on SDK, so each CPU cycle is 8ns
+// Need at least 150ns of wait for the longest SPI action
+// Altho SPI hardware should handle that
 
-static inline void reg_write(uint8_t reg, uint8_t data){
+static inline void reg_write(uint8_t reg, uint8_t data) {
     uint8_t msg[2];
-    msg[0] = reg; //reg will always have the top 2 bits 0, ensuring write mode with 1 byte
+    msg[0] = reg; // reg will always have the top 2 bits 0, ensuring write mode with 1 byte
     msg[1] = data;
 
     gpio_put(PIN_CS, 0);
@@ -53,9 +53,9 @@ static inline void reg_write(uint8_t reg, uint8_t data){
     busy_wait_at_least_cycles(20);
 }
 
-static inline void reg_read(uint8_t reg, uint8_t *buffer, uint8_t nbytes){
+static inline void reg_read(uint8_t reg, uint8_t *buffer, uint8_t nbytes) {
     uint8_t msg[1];
-    msg[0] = (0b1 << 7) | (((nbytes > 1) ? 1 : 0) << 6) | reg; //Ensures top bit is 1, and second is 0 or 1
+    msg[0] = (0b1 << 7) | (((nbytes > 1) ? 1 : 0) << 6) | reg; // Ensures top bit is 1, and second is 0 or 1
 
     gpio_put(PIN_CS, 0);
     busy_wait_at_least_cycles(20);
@@ -67,49 +67,50 @@ static inline void reg_read(uint8_t reg, uint8_t *buffer, uint8_t nbytes){
     busy_wait_at_least_cycles(20);
 }
 
-void read_state_off(void){
+void read_state_off(void) {
     uint8_t power_setting;
-    reg_read(REG_POWER_CTL, &power_setting, 1); //Obtain current Power Control setting
-    power_setting = power_setting & ~(0b1 << 3); //Change Power Control Measure bit to 0 to end data collecition, see pg. 
+    reg_read(REG_POWER_CTL, &power_setting, 1);  // Obtain current Power Control setting
+    power_setting = power_setting & ~(0b1 << 3); // Change Power Control Measure bit to 0 to end data collecition, see pg.
     reg_write(REG_POWER_CTL, power_setting);
 }
- 
-void read_state_on(void){    
-    reg_write(REG_DATA_RATE, DATA_RATE); //Sets Data Output Rate
-    reg_write(REG_DATA_FORMAT, DATA_FORMAT); //Sets Data Format
-    
+
+void read_state_on(void) {
+    reg_write(REG_DATA_RATE, DATA_RATE);     // Sets Data Output Rate
+    reg_write(REG_DATA_FORMAT, DATA_FORMAT); // Sets Data Format
+
     uint8_t power_setting;
-    reg_read(REG_POWER_CTL, &power_setting, 1); //Obtain current Power Control setting
-    power_setting = power_setting | (0b1 << 3); //Change Power Control Measure bit to 1 to begin data collecition, see pg. 
+    reg_read(REG_POWER_CTL, &power_setting, 1); // Obtain current Power Control setting
+    power_setting = power_setting | (0b1 << 3); // Change Power Control Measure bit to 1 to begin data collecition, see pg.
     reg_write(REG_POWER_CTL, power_setting);
 }
-        
-void intr_state_off(void){
-    reg_write(REG_INT_ENABLE, 0); //A value of 0 disables all interrupts
-}        
 
-void intr_state_on(void){
-    reg_write(REG_INT_MAP, INT_MAP); //Sets INT pin
-    reg_write(REG_THRESH_ACT, THRESH_ACT); //Sets threshold
-    reg_write(REG_FIFO_MODE, FIFO_MODE); //Sets FIFO mode
-    reg_write(REG_ACT_CTL, ACT_CTL); //Sets axes for monitoring
-
-    reg_write(REG_INT_ENABLE, INT_ENABLE); //Enables interrupts. Must go last, see doc pg. 18
+void intr_state_off(void) {
+    reg_write(REG_INT_ENABLE, 0); // A value of 0 disables all interrupts
 }
 
-void reset_intr_state(void){
-    uint8_t foo; //Dummy variable for a buffer
+void intr_state_on(void) {
+    reg_write(REG_INT_MAP, INT_MAP);       // Sets INT pin
+    reg_write(REG_THRESH_ACT, THRESH_ACT); // Sets threshold
+    reg_write(REG_FIFO_MODE, FIFO_MODE);   // Sets FIFO mode
+    reg_write(REG_ACT_CTL, ACT_CTL);       // Sets axes for monitoring
+
+    reg_write(REG_INT_ENABLE, INT_ENABLE); // Enables interrupts. Must go last, see doc pg. 18
+}
+
+void reset_intr_state(void) {
+    uint8_t foo; // Dummy variable for a buffer
     reg_read(REG_INT_SOURCE, &foo, 1);
 }
 
-uint8_t reading_validation(void){
+uint8_t reading_validation(void) { // TODO: make better
     uint8_t foo;
     reg_read(REG_DEVID, &foo, 1);
     return foo;
 }
 
-void fetch_data(uint8_t *buffer){ //Optimized for speed, data needs further handling, used in main loop
-    for(int i = 0; i < STRIKE_SAMPLES; i++, buffer += BPS){
+void fetch_data(uint8_t *buffer) { // Optimized for speed, data needs further handling, used in main loop
+    for (int i = 0; i < STRIKE_SAMPLES; i++, buffer += BPS) {
         reg_read(REG_DATAX0, buffer, BPS);
+        busy_wait_us(1250); // 1/(3200 hz) in us is 312.5
     }
 }
